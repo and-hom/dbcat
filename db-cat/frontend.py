de7159504e087154d0f943276dd25ed5c404aca4
Андрей Хомяков <and.hom@gmail.com>
2014-07-05 23:49:07 +0400

    Removed fake filter generator

--- db-cat/frontend.py
+++ db-cat/frontend.py
@@ -17,27 +17,6 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 JINJA_ENVIRONMENT.filters['filter_type'] = filter_type
 
 
-def fake_filters():
-    logging.debug('Creating fake filters')
-    sf1 = SelectFilter()
-    sf1.name = 'Selector1'
-    sf1.description = 'Selector1'
-    sf1.options = ['v1', 'v2']
-    sf1.put()
-
-    rf1 = IntRangeFilter()
-    rf1.name = 'Some val'
-    rf1.min = 0
-    rf1.max = 10
-    rf1.put()
-
-    bf1 = BooleanFilter()
-    bf1.name = 'Flag'
-    bf1.put()
-
-
-# fake_filters()
-
 frontend = webapp2.WSGIApplication([
                                        ('/', MainPage),
                                        ('/filter', FilterChangeRequestPage),
