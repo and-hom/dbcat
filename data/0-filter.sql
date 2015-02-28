e2c354bec729ff13243080549042a694e0f73bf0
Андрей Хомяков <and-hom@yandex-team.ru>
2015-03-01 00:27:16 +0300

    Refs #2 Multichoice select filters. Field "multichoice" added into filters init sql script

--- data/0-filter.sql
+++ data/0-filter.sql
@@ -33,13 +33,14 @@ $$
                 description text,
                 priority integer,
                 required boolean,
+                multichoice boolean,
                 options character varying(64)[][]) RETURNS VOID
 AS $$
 DECLARE
    option   varchar[];
 BEGIN
     INSERT INTO frontend_filter(code,name,description,priority) VALUES (code,name,description,priority);
-    INSERT INTO frontend_selectfilter(filter_ptr_id,required) VALUES (code,required);
+    INSERT INTO frontend_selectfilter(filter_ptr_id,required,multichoice) VALUES (code,required,multichoice);
     FOREACH option SLICE 1 IN ARRAY options
     LOOP
 	INSERT INTO frontend_selectoption(filter_id,code,name) VALUES (code,option[1],option[2]);
@@ -52,28 +53,28 @@ DO
 $$
 DECLARE
 BEGIN
-   execute select_filter('paradigm','Парадигма','',0, false,
+   execute select_filter('paradigm','Парадигма','',0, false, true,
 			array[['rel','Реляционная'],['keyval','Ключ-Значение'],['doc','Документно-ориентированная'],['col','Семейство столбцов'],['graph','Граф']]);
-   execute select_filter('atomic','Атомарность','',10, false,
+   execute select_filter('atomic','Атомарность','',10, false, false,
 			array[['tx','Транзакции'],['cas','Одна запись атомарна'],['no','Нет']]);			
-   execute select_filter('isolation','Изоляция','',20,false,
+   execute select_filter('isolation','Изоляция','',20,false, false,
 			array[['block','Блокировки'],['ver','Версии']]);
 			
    execute boolean_filter('shard','Шардирование','',30);
-   execute select_filter('repl','Репликация','',40, false,
+   execute select_filter('repl','Репликация','',40, false, false,
 			array[['ms','Master-Slave'],['p2p','Peer-to-Peer'],['other','Другая'],['no','Нет']]);
    execute boolean_filter('ttl','Удаление устаревших записей','',5);
 
-   execute select_filter('lic','Лицензия','',60, false,
+   execute select_filter('lic','Лицензия','',60, false, false,
 			array[['comm','Коммерческая'],['apache2','Apache 2'],['agpl','AGPL'],['gpl2','GPLv2'],['otherfree','Другая cвободная'],['edu','Образовательная'],['other','Другая']]);
    	
-   execute select_filter('idgen','Генерация id','',70, false,
+   execute select_filter('idgen','Генерация id','',70, false, false,
 			array[['seq','Последоватльности'],['autoinc','Автоинкрементное поле'],['uuid','UUID'],['other','Другая'],['no','Отсутствует']]);
-   execute select_filter('sort','Сортировка','',80, false,
+   execute select_filter('sort','Сортировка','',80, false, false,
 			array[['any','По любому полю'],['idx','Только по индексу'],['id','Только по id'],['no','Невозможна']]);
-   execute select_filter('access','Доступ','',90, false,
+   execute select_filter('access','Доступ','',90, false, false,
 			array[['net','По сети'],['builtin','Встраиваемая в приложение']]);
-   execute select_filter('codelang','Исполняемый код СУБД','',100, false,
+   execute select_filter('codelang','Исполняемый код СУБД','',100, false, false,
 			array[['native','Нативный код'],['java','Java'],['erlang','Erlang']]);
    	
 END;
